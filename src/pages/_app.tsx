bbfd135f4f521df458ee747979ac703c742cdd3d
ankush170 <f20191095@goa.bits-pilani.ac.in>
2024-11-01 08:56:19 +0530

    desc change 2
    Signed-off-by: ankush170 <f20191095@goa.bits-pilani.ac.in>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>OnFinance AI</title>
-        <meta name="description" content="OnFinance AI - Explainable AI Agents for financial services. Get verifiable and controllable responses from LLMs on financial documents like regulatory circulars." />
+        <meta name="description" content="OnFinance AI - Explainable AI Agents for financial services. Get verifiable and controllable responses from LLMs on financial documents like regulatory circulars. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
